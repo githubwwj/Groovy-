fea27ca05e5afd318c16283abada16793a3d79cc
wwj <wwj_computeryes@163.com>
2017-12-05 13:44:35 +0800

    创建项目

---
+++ src/Hello.groovy
@@ -0,0 +1,18 @@
+
+def add(a,b){
+    a+b
+    //默认它自己会加 return
+}
+
+//int add(int a,int b){
+//    return a+b;
+//}
+
+def total=add(5,3)
+
+def s1="Hello "
+def s2="World"
+println(total)
+
+println(s1+s2)
+//CTRL + SHIFT + F10
